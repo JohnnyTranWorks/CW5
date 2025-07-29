81427f0b8c736bb99365b3608d5db0c2c06038cf
Johnny Tran <johnnytranschool@gmail.com>
2025-07-28 22:32:52 -0400

    Initial commit

---
+++ src/FilteredList.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import List from './List';
+
+class FilteredList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      type: "All"
+    };
+  }
+
+  onSearch = (event) => {
+    this.setState({ search: event.target.value.trim().toLowerCase() });
+  }
+
+  onFilter = (event) => {
+    this.setState({ type: event });
+  }
+
+  filterItem = (item) => {
+    const matchesSearch = item.name.toLowerCase().includes(this.state.search);
+    const matchesType = this.state.type === "All" || item.type === this.state.type;
+    return matchesSearch && matchesType;
+  }
+
+  render() {
+    return (
+      <div className="filter-list">
+        <DropdownButton title="Filter by Type" id="dropdown-basic-button" onSelect={this.onFilter}>
+          <Dropdown.Item eventKey="All">All</Dropdown.Item>
+          <Dropdown.Item eventKey="Fruit">Fruit</Dropdown.Item>
+          <Dropdown.Item eventKey="Vegetable">Vegetable</Dropdown.Item>
+        </DropdownButton>
+
+        <input type="text" placeholder="Search" onChange={this.onSearch} />
+        <List items={this.props.items.filter(this.filterItem)} />
+      </div>
+    );
+  }
+}
+
+export default FilteredList;
+
+
